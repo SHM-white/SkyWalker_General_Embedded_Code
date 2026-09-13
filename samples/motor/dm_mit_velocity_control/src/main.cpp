f24866fe1a3a26f8c8cbd968279a02710224275a
SHM_white <1131287310a@gmail.com>
2026-09-13 22:17:15 +0800

    可以使用lambda函数初始化

--- samples/motor/dm_mit_velocity_control/src/main.cpp
+++ samples/motor/dm_mit_velocity_control/src/main.cpp
@@ -33,50 +33,6 @@ float targetVelocityRadS() {
     return 2.0f;
 }
 
-control_motor_velocity_config makeVelocityLoopConfig() {
-    control_motor_velocity_config config{};
-    config.regulator.feedback = {
-        .kp = 0.02f,
-        .ki = 0.15f,
-        .kd = 0.0f,
-        .derivative_tau_s = 0.0f,
-        .integral_min = -0.3f,
-        .integral_max = 0.3f,
-        .output_min = -kSoftwareTorqueAbsMaxNm,
-        .output_max = kSoftwareTorqueAbsMaxNm,
-        .deadband = 0.0f,
-        .dt_min_s = 0.001f,
-        .dt_max_s = 0.020f,
-    };
-    config.regulator.feedforward = {
-        .k_bias = 0.0f,
-        .k_static = 0.0f,
-        .k_velocity = 0.0f,
-        .k_acceleration = 0.0f,
-        .k_gravity = 0.0f,
-        .velocity_epsilon = 0.0f,
-        .acceleration_epsilon = 0.0f,
-        .gravity_model = CONTROL_GRAVITY_NONE,
-    };
-    config.reference_slew = {
-        .rising_rate_per_s = 2.0f,
-        .falling_rate_per_s = 2.0f,
-    };
-    config.measurement_filter_tau_s = 0.02f;
-    config.soft_deadband_rad_s = 0.02f;
-    config.requested_velocity_abs_max_rad_s = kRequestedVelocityAbsMaxRadS;
-    config.effort_abs_max = kSoftwareTorqueAbsMaxNm;
-    return config;
-}
-
-skywalker::control::VelocityMotor::Config makeMotorConfig() {
-    skywalker::control::VelocityMotor::Config config{};
-    config.loop = makeVelocityLoopConfig();
-    config.effort_unit = skywalker::control::EffortUnit::NewtonMeter;
-    config.safety = {kVelocityCutoffRadS, kTemperatureCutoffC};
-    return config;
-}
-
 } // namespace
 
 int main() {
@@ -86,7 +42,48 @@ int main() {
     // Bus and UART callbacks retain these objects after an early return.
     static skywalker::control::DmMotorBackend backend{DEVICE_DT_GET(MOTOR0_NODE),
                                                       skywalker::samples::dm::enableMotorPower};
-    static skywalker::control::VelocityMotor motor{backend, makeMotorConfig()};
+    static skywalker::control::VelocityMotor
+        motor{backend, []() {
+                  skywalker::control::VelocityMotor::Config config{};
+                  config.loop = []() {
+                      control_motor_velocity_config config{};
+                      config.regulator.feedback = {
+                          .kp = 0.02f,
+                          .ki = 0.15f,
+                          .kd = 0.0f,
+                          .derivative_tau_s = 0.0f,
+                          .integral_min = -0.3f,
+                          .integral_max = 0.3f,
+                          .output_min = -kSoftwareTorqueAbsMaxNm,
+                          .output_max = kSoftwareTorqueAbsMaxNm,
+                          .deadband = 0.0f,
+                          .dt_min_s = 0.001f,
+                          .dt_max_s = 0.020f,
+                      };
+                      config.regulator.feedforward = {
+                          .k_bias = 0.0f,
+                          .k_static = 0.0f,
+                          .k_velocity = 0.0f,
+                          .k_acceleration = 0.0f,
+                          .k_gravity = 0.0f,
+                          .velocity_epsilon = 0.0f,
+                          .acceleration_epsilon = 0.0f,
+                          .gravity_model = CONTROL_GRAVITY_NONE,
+                      };
+                      config.reference_slew = {
+                          .rising_rate_per_s = 2.0f,
+                          .falling_rate_per_s = 2.0f,
+                      };
+                      config.measurement_filter_tau_s = 0.02f;
+                      config.soft_deadband_rad_s = 0.02f;
+                      config.requested_velocity_abs_max_rad_s = kRequestedVelocityAbsMaxRadS;
+                      config.effort_abs_max = kSoftwareTorqueAbsMaxNm;
+                      return config;
+                  }();
+                  config.effort_unit = skywalker::control::EffortUnit::NewtonMeter;
+                  config.safety = {kVelocityCutoffRadS, kTemperatureCutoffC};
+                  return config;
+              }()};
     static Vofa vofa{};
     vofa_init(&vofa, uart);
     int ret = motor.configure();
